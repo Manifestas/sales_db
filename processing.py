eb6ca045bf9fae353e9fc5167e0c9fc2ab233a9b
ms <doslovno@gmail.com>
2024-01-09 14:52:00 +0300

    change drop args

--- processing.py
+++ processing.py
@@ -4,8 +4,8 @@ import pandas as pd
 import numpy as np
 from db import *
 
-
 TEMP_DIR_NAME = "./temp/"
+
 
 def export_tables_to_csv():
     table_to_sql(division_table_name)
@@ -18,7 +18,7 @@ def export_tables_to_csv():
 def process_table_sales():
     cnx = sqlite3.connect(sqlite_db_file)
     sales = pd.read_sql_query(f"SELECT * FROM {sales_table_name}", cnx)
-    sales.drop([doc_number_col, organization_id_col, operator_id_col, product_cost_price_col], axis=1, inplace=True)
+    sales.drop(columns=[doc_number_col, organization_id_col, operator_id_col, product_cost_price_col], inplace=True)
     sales.replace([None], np.nan, inplace=True)
     sales = sales.astype({division_id_col: "Int64", tt_id_col: "Int64", product_id_col: "Int64"})
 
@@ -34,15 +34,15 @@ def process_table_sales():
 
     # дополнительная таблица для уменьшения размера основной
     uniq_combo_id = sales_group.drop_duplicates(subset=[doc_date_col, ta_id_col, division_id_col, tt_id_col])
-    uniq_combo_id = uniq_combo_id.drop(labels=[ta_cell_number_col,
-                                               product_id_col,
-                                               number_of_sales_col,
-                                               sum_of_sales_col,
-                                               product_control_cost_price_col], axis=1)
+    uniq_combo_id = uniq_combo_id.drop(columns=[ta_cell_number_col,
+                                                product_id_col,
+                                                number_of_sales_col,
+                                                sum_of_sales_col,
+                                                product_control_cost_price_col])
     combo_id = "combo_id"
     uniq_combo_id[combo_id] = range(1, len(uniq_combo_id) + 1)
     uniq_combo_id.to_csv(f"{TEMP_DIR_NAME}{combo_id}.csv")
-    
+
     sales_group = sales_group.merge(uniq_combo_id, how='left')
     sales_group.drop(labels=[doc_date_col, ta_id_col, division_id_col, tt_id_col], axis=1, inplace=True)
     sales_group.to_csv(f"{TEMP_DIR_NAME}{sales_table_name}.csv", index=False)
